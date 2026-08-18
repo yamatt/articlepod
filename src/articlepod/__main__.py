21b01cd02d8a7d9791408d1db5704fc419419b88
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2026-08-18 08:02:11 +0000

    configure number of episodes in rss feed

--- src/articlepod/__main__.py
+++ src/articlepod/__main__.py
@@ -103,13 +103,14 @@ def rss():
 
 @rss.command()
 @click.argument("episode_dir", type=click.Path(exists=True))
-def generate_rss(episode_dir: str):
+@click.option("--episodes", default=10, type=int, help="Number of episodes to include in the RSS feed.")
+def generate_rss(episode_dir: str, episodes: int = 10):
     """Generate RSS feed from episode directory."""
     episodes = sorted([
         json.load(open(os.path.join(episode_dir, f)))
         for f in os.listdir(episode_dir)
         if os.path.isfile(os.path.join(episode_dir, f)) and f.endswith(".json")
-    ], key=lambda x: x["added"], reverse=True)[:10]  # Get the 10 most recent episodes
+    ], key=lambda x: x["added"], reverse=True)[:episodes]  # Get the specified number of most recent episodes
 
     click.echo(generate_rss_feed(episodes, "Articlepod", "A podcast about interesting articles.", "https://github.com/yamatt/articlepod", "https://yamatt.github.io/articlepod/rss.xml"))
 
